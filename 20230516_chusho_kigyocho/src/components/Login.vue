fd0d681481a618f23c4c4daf5daac301ed1573b2
20230516_macos_git_user <add-122@hotmail.com>
2023-05-16 15:04:20 +0900

    有编辑，要安装axios

--- 20230516_chusho_kigyocho/src/components/Login.vue
+++ 20230516_chusho_kigyocho/src/components/Login.vue
@@ -1,9 +1,13 @@
 <template>
-
+    <div>
+        <label>用户名</label><input type="text" />
+        <label>密码</label><input type="password" />
+        <input type="radio" name="usertype" value="manager"/>管理员
+        <input type="radio" name="usertype" value="normaluser"/>普通用户
+        <input type="button" value="提交" />
+    </div>
 </template>
 <script>
-
+    
 </script>
-<style>
-
-</style>
+<style></style>
---
+++ 20230516_chusho_kigyocho/src/router/index.js
@@ -0,0 +1,33 @@
+import VueRouter from 'vue-router';
+import Vue from 'vue';
+
+Vue.use(VueRouter);
+
+import ContentForManager from '../components/ContentForManager';
+import ContentForNormaluser from '../components/ContentForNormaluser'
+import Login from '../components/Login'
+
+const routes = [
+    {
+        path: '/content-for-manager',
+        name: 'ContentForManager',
+        component: ContentForManager
+    },
+    {
+        path: '/content-for-normaluser',
+        name: 'ContentForNormaluser',
+        component: ContentForNormaluser
+    },
+    {
+        path: '/login',
+        name: 'Login',
+        component: Login
+    }
+];
+
+const router = new VueRouter({
+    routes,
+    mode: 'history'
+});
+
+export default router;
